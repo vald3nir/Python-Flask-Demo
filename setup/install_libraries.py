a6e85f4255156ea05fed8e804261f78889130e26
Valdenir Severino <vald3nir@gmail.com>
2021-07-25 10:42:58 -0300

    add script to build the project

--- setup/install_libraries.py
+++ setup/install_libraries.py
@@ -3,21 +3,16 @@
 
 from subprocess import call
 
-import pkg_resources
-
 libraries = [
     # Flask libraries
-    "Flask", "Flask-Cors",
+    "Flask", "Flask-Cors", "Flask-SQLAlchemy",
     # Databases
     "Flask-PyMongo", "pymongo[srv]",
+    # Web Token
+    "jwt",
     # Geolocation
     "geopy",
     # Translate strings
     "textblob",
 ]
 call("pip install " + ' '.join(libraries), shell=True)
-
-packages = [dist.project_name for dist in pkg_resources.working_set]
-call("pip install --upgrade " + ' '.join(packages), shell=True)
-
-call("pip freeze > ../requirements.txt", shell=True)
